e1c8a443a0e99a1b6a084e6d415df9494d9b67da
Gabriel Laguyonie <glaguyon@3E5.42angouleme.fr>
2024-10-11 18:01:48 +0200

    added cpper megaphone

--- cpp00/ex00/src/megaphone.cpp
+++ cpp00/ex00/src/megaphone.cpp
@@ -6,13 +6,30 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:27:24 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/10/11 16:28:35 by glaguyon         ###   ########.fr       */
+/*   Updated: 2024/10/11 17:58:19 by glaguyon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
-int	main()
+#define skibidi std
+
+int	main(int, char **av)
 {
-	std::cout << "a" << "\n";
+	++av;
+	if (!*av)
+		skibidi::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		for (; *av; ++av)
+		{
+			skibidi::string	s = *av;
+
+			for (skibidi::string::iterator it = s.begin(); it != s.end(); ++it)
+				*it = skibidi::toupper(*it);
+			skibidi::cout << s;
+		}
+		skibidi::cout << "\n";
+	}
 }
